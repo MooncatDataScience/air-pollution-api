0cd653ef388073f96cc99adf329d4d31d9b09619
klas9802 <klas9806@gmail.com>
2023-03-08 09:56:07 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# 使用pytorch的CPU版本
+FROM pytorch/pytorch:latest
+
+# 設置工作目錄
+WORKDIR /myapp
+
+# 複製PyTorch檔案到容器中
+COPY test.py .
+
+# 安裝其他必要的Python套件
+RUN pip install pandas matplotlib scikit-learn
+
+# 指定容器啟動時運行的命令
+CMD [ "python", "test.py" ]
---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+import csv
+import time
+
+def get_aqi_data():
+    url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+    data = requests.get(url)
+    data_json = data.json()
+    
+    aqi_data = []
+    for i in data_json['records']:
+        if i['county'] == '臺中市':
+            row = {
+                'site': i['sitename'],
+                'county': i['county'],
+                'pollutant': i['pollutant'],
+                'so2': i['so2'],
+                'co': i['co'],
+                'o3':i['o3'],
+                'o3_8hr': i['o3_8hr'],
+                'pm10': i['pm10'],
+                'pm2.5': i['pm2.5'],
+                'no2': i['no2'],
+                'nox': i['nox'],
+                'no': i['no'],
+                'wind_speed': i['wind_speed'],
+                'wind_direc': i['wind_direc'],
+                'publishtime': i['publishtime'],
+                'co_8hr': i['co_8hr'],
+                'pm2.5_avg': i['pm2.5_avg'],
+                'pm10_avg': i['pm10_avg'],
+                'so2_avg': i['so2_avg'],
+                'longitude': i['longitude'],
+                'latitude': i['latitude'],
+                'aqi': i['aqi'],
+                'status': i['status'],
+            }
+            aqi_data.append(row)
+    
+    return aqi_data
+
+while True:
+    aqi_data = get_aqi_data()
+    
+    with open('aqi_data.csv', mode='a', newline='') as csvfile:
+        fieldnames = ['site', 'county', 'pollutant', 'so2', 'co','o3','o3_8hr', 'pm10', 'pm2.5', 'no2', 'nox', 'no'
+, 'wind_speed', 'wind_direc', 'publishtime', 'co_8hr', 'pm2.5_avg', 'pm10_avg', 'so2_avg', 'longitude', 'latitude', 'aqi', 'status',]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        for row in aqi_data:
+            writer.writerow(row)
+    
+    now = time.ctime()
+    print("寫入成功", now)
+    
+    time.sleep(3600) # 暫停 3600 秒，也就是 1 小時
