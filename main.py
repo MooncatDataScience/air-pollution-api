1f2f652a6c632e5c8e04ad255a18249003d481d9
Mooncat <48311280+klas9802@users.noreply.github.com>
2023-03-08 16:38:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 import time
 
 def get_aqi_data():
-    url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+    url = ''
     data = requests.get(url)
     data_json = data.json()
     
